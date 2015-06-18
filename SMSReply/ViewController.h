447ebd11d033c994a61d5e3cd448b795a2e25ed3
Ryan Wang <wanglun02@gmail.com>
2015-06-19 01:12:46 +0800

    Initial Commit

---
+++ SMSReply/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SMSReply
+//
+//  Created by Ryan Wang on 15/6/19.
+//  Copyright (c) 2015年 Ryan Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SMSReplyTests/SMSReplyTests.m
@@ -0,0 +1,40 @@
+//
+//  SMSReplyTests.m
+//  SMSReplyTests
+//
+//  Created by Ryan Wang on 15/6/19.
+//  Copyright (c) 2015年 Ryan Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SMSReplyTests : XCTestCase
+
+@end
+
+@implementation SMSReplyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
